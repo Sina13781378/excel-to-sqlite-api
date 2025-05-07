aedc494a316e90e5ede4a006ef5a788c5885d21b
Sina13781378 <sinadorostkar1378@gmail.com>
2025-05-08 01:41:39 +0330

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,4 @@ def get_table_columns(conn, table):
     return [row[1] for row in cursor.fetchall()]
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(host='0.0.0.0', port=10000)
